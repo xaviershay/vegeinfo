b8e15d1789a219bc62bf58acc417da0faa40a93b
Xavier Shay <xavier@rhnh.net>
2008-12-26 10:23:18 -0800

    Refactor for nicer array methods

--- vegeinfo.rb
+++ vegeinfo.rb
@@ -2,13 +2,18 @@ require 'rubygems'
 require 'sinatra'
 require 'json'
 
-class Array
+module BonusArrayMethods
   def collect_with_index
     ret = []
     each_with_index do |item, index|
       ret << yield(item, index)
     end
     ret
+  end
+
+  def inject_with_index(injected)
+    each_with_index{ |obj, index| injected = yield(injected, obj, index) }
+    injected
   end
 end
 
