ab6ee45c9bf5a5bde426557cd39d03cf82f99069
Xavier Shay <xavier@rhnh.net>
2008-12-26 10:20:22 -0800

    A stacked bar chart for water

--- vegeinfo.rb
+++ vegeinfo.rb
@@ -2,6 +2,16 @@ require 'rubygems'
 require 'sinatra'
 require 'json'
 
+class Array
+  def collect_with_index
+    ret = []
+    each_with_index do |item, index|
+      ret << yield(item, index)
+    end
+    ret
+  end
+end
+
 get '/' do
   erb :cost
 end
