b22c11b49ef1292399f5772e5742caf97df69211
Xavier Shay <xavier@rhnh.net>
2009-01-03 14:06:57 -0800

    Add formatNumber function to tufteBar

--- public/javascripts/jquery.tufte-graph.js
+++ public/javascripts/jquery.tufte-graph.js
@@ -20,6 +20,21 @@
     }
   }
 
+  $.tufteBar = {
+    formatNumber: function(nStr) {
+      // http://www.mredkj.com/javascript/nfbasic.html
+      nStr += '';
+      x = nStr.split('.');
+      x1 = x[0];
+      x2 = x.length > 1 ? '.' + x[1] : '';
+      var rgx = /(\d+)(\d{3})/;
+      while (rgx.test(x1)) {
+        x1 = x1.replace(rgx, '$1' + ',' + '$2');
+      }
+      return x1 + x2;
+    }
+  }
+
   //
   // Private functions
   //
